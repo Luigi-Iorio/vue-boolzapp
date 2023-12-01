687e33099b1d9068a3957c232863adf0b23461f0
Luigi-Iorio <luigi0iorio@gmail.com>
2023-12-01 17:19:30 +0100

    feat: info su messaggi

--- milestone5/js/main.js
+++ milestone5/js/main.js
@@ -14,16 +14,19 @@ createApp({
               date: "10/01/2020 15:30:55",
               message: "Hai portato a spasso il cane?",
               status: "sent",
+              menuTendina: {},
             },
             {
               date: "10/01/2020 15:50:00",
               message: "Ricordati di stendere i panni",
               status: "sent",
+              menuTendina: {},
             },
             {
               date: "10/01/2020 16:15:22",
               message: "Tutto fatto!",
               status: "received",
+              menuTendina: {},
             },
           ],
         },
@@ -36,16 +39,19 @@ createApp({
               date: "20/03/2020 16:30:00",
               message: "Ciao come stai?",
               status: "sent",
+              menuTendina: {},
             },
             {
               date: "20/03/2020 16:30:55",
               message: "Bene grazie! Stasera ci vediamo?",
               status: "received",
+              menuTendina: {},
             },
             {
               date: "20/03/2020 16:35:00",
               message: "Mi piacerebbe ma devo andare a fare la spesa.",
               status: "sent",
+              menuTendina: {},
             },
           ],
         },
@@ -58,16 +64,19 @@ createApp({
               date: "28/03/2020 10:10:40",
               message: "La Marianna va in campagna",
               status: "received",
+              menuTendina: {},
             },
             {
               date: "28/03/2020 10:20:10",
               message: "Sicuro di non aver sbagliato chat?",
               status: "sent",
+              menuTendina: {},
             },
             {
               date: "28/03/2020 16:15:22",
               message: "Ah scusa!",
               status: "received",
+              menuTendina: {},
             },
           ],
         },
@@ -80,11 +89,13 @@ createApp({
               date: "10/01/2020 15:30:55",
               message: "Lo sai che ha aperto una nuova pizzeria?",
               status: "sent",
+              menuTendina: {},
             },
             {
               date: "10/01/2020 15:50:00",
               message: "Si, ma preferirei andare al cinema",
               status: "received",
+              menuTendina: {},
             },
           ],
         },
@@ -97,11 +108,13 @@ createApp({
               date: "10/01/2020 15:30:55",
               message: "Ricordati di chiamare la nonna",
               status: "sent",
+              menuTendina: {},
             },
             {
               date: "10/01/2020 15:50:00",
               message: "Va bene, stasera la sento",
               status: "received",
+              menuTendina: {},
             },
           ],
         },
@@ -114,16 +127,19 @@ createApp({
               date: "10/01/2020 15:30:55",
               message: "Ciao Claudia, hai novità?",
               status: "sent",
+              menuTendina: {},
             },
             {
               date: "10/01/2020 15:50:00",
               message: "Non ancora",
               status: "received",
+              menuTendina: {},
             },
             {
               date: "10/01/2020 15:51:00",
               message: "Nessuna nuova, buona nuova",
               status: "sent",
+              menuTendina: {},
             },
           ],
         },
@@ -136,11 +152,13 @@ createApp({
               date: "10/01/2020 15:30:55",
               message: "Fai gli auguri a Martina che è il suo compleanno!",
               status: "sent",
+              menuTendina: {},
             },
             {
               date: "10/01/2020 15:50:00",
               message: "Grazie per avermelo ricordato, le scrivo subito!",
               status: "received",
+              menuTendina: {},
             },
           ],
         },
@@ -153,16 +171,19 @@ createApp({
               date: "10/01/2020 15:30:55",
               message: "Ciao, andiamo a mangiare la pizza stasera?",
               status: "received",
+              menuTendina: {},
             },
             {
               date: "10/01/2020 15:50:00",
               message: "No, l'ho già mangiata ieri, ordiniamo sushi!",
               status: "sent",
+              menuTendina: {},
             },
             {
               date: "10/01/2020 15:51:00",
               message: "OK!!",
               status: "received",
+              menuTendina: {},
             },
           ],
         },
@@ -183,6 +204,7 @@ createApp({
           date: "00/00/0000 00:00:00",
           message: "ok",
           status: "received",
+          menuTendina: {},
         });
       }, 1000);
     },
@@ -192,6 +214,7 @@ createApp({
           date: "00/00/0000 00:00:00",
           message: this.testoInput,
           status: "sent",
+          menuTendina: {},
         });
         this.rispostaPosticipata();
       }
@@ -218,5 +241,15 @@ createApp({
         });
       }
     },
+    menuMessaggio(index) {
+      this.contacts[this.contattoAttivo].messages[index].menuTendina.push({
+        info: "Message info",
+        delete: "Delete message",
+        attivo: true,
+      });
+    },
+    // cancellaMessaggio(index) {
+    //   this.contacts[this.contattoAttivo].messages[index]menuTendina.slice(index, 1);
+    // },
   },
 }).mount("#app");
